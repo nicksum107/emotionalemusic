0eadff8ffb3be57c14a86b83b106f31c95ea347f
nicksum107 <nicksum107@gmail.com>
2021-05-13 18:50:11 -0400

    more collision

--- src/components/objects/Piano/Piano.js
+++ src/components/objects/Piano/Piano.js
@@ -2,9 +2,11 @@ import { Mesh } from 'three';
 import { Group } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import MODEL from './model.gltf';
-
+import knock from '../../sounds/knock.mp3'
+import { AudioLoader } from 'three';
+import { PositionalAudio } from 'three';
 class Piano extends Group {
-    constructor() {
+    constructor(audiolist) {
         // Call parent Group() constructor
         super();
 
@@ -20,17 +22,37 @@ class Piano extends Group {
         loader.load(MODEL, (gltf) => {
             // this.add(gltf.scene);
             gltf.scene.traverse(function(child) {
+                // only add the mesh
                 if (child instanceof Mesh) {
-                    // console.log(child)
                     p.mesh = child
                     temp = child 
                     p.add(child)
                 }
             });
         });
+        this.sounds = new Array(15)
+        const audioLoader = new AudioLoader()
+        for (let i = 0; i < this.sounds.length; i++) {
+            this.sounds[i] = new PositionalAudio(audiolist)
+            
+            audioLoader.load(knock, function(buffer){
+                p.sounds[i].setBuffer(buffer)
+                p.sounds[i].setRefDistance(20)
+                p.sounds[i].duration = 0.7 // Cut off sound at 0.7 seconds
+            })
+        }
+    }
+    playsound(velocity) {
+        for (let s of this.sounds){ 
+            if (!s.isPlaying) {
+                // volume goes from 0 to 20 without sounding bad
+                // console.log(scaledvel, s.getVolume())
 
-        // console.log(temp)
-        // this.add(temp)
+                s.setVolume(-1 * velocity.length() *1)
+                s.play()
+                break 
+            }
+        }
     }
 }
 
